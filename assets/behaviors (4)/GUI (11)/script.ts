6e1aa0da283c186e1b62b17971ad856675ad41bd
ClementRivaille <rivaille.clement@orange.fr>
2016-08-25 23:22:59 +0200

    feat: animation on messages

--- assets/behaviors (4)/GUI (11)/script.ts
+++ assets/behaviors (4)/GUI (11)/script.ts
@@ -3,14 +3,29 @@ class GUIBehavior extends Sup.Behavior {
   private queueCount: number = 0;
   private nextDialog: number = 0;
   private dialogs: Array<string>;
+  
+  private positions = {off:-6, on: -4};
+  private move: number = 0;
 
   private pureGameplay: boolean = false;
 
   awake() {
+    // Initialize messages text box
     this.messages = this.actor.getChild('messages');
     this.messages.textRenderer.setText('');
+    this.messages.setLocalY(this.positions.off);
     
     this.dialogs = DIALOGS;
+  }
+
+  update() {
+    // Animates messages between positions
+    if (this.move !== 0) {
+      this.messages.moveLocalY(this.move);
+      if (comparePositions(this.messages.getLocalY(), this.positions.on) || comparePositions(this.messages.getLocalY(), this.positions.off)) {
+        this.move = 0;
+      }
+    }
   }
 
   printDialog() {
@@ -18,6 +33,9 @@ class GUIBehavior extends Sup.Behavior {
       // Display next validation message
       this.messages.textRenderer.setText(this.dialogs[this.nextDialog]);
       this.nextDialog = Math.min(this.nextDialog + 1, this.dialogs.length -1);
+      // Little animation
+      this.messages.setLocalY(this.positions.off);
+      this.move = 0.1;
     }
     else {
       // On pure gameplay mode, only say 'WIN'
@@ -27,11 +45,18 @@ class GUIBehavior extends Sup.Behavior {
     // Wait before removing text
     this.queueCount += 1;
     Sup.setTimeout(3000, () => {
+      // Manage several stimultaneous timers, only the last one will hide the text
       this.queueCount -= 1;
       if (this.queueCount <= 0) {
-        this.messages.textRenderer.setText('');
+        // Move text downward
+        this.move = -0.1;
       }
     })
   }
 }
 Sup.registerBehavior(GUIBehavior);
+
+/** Utilitary function to compare position with an error margin */
+function comparePositions(pos1: number, pos2: number) {
+  return Math.abs(pos1 - pos2) < 0.001;
+}
